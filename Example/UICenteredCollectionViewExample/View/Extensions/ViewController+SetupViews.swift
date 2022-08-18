50fe6035bb2caa2de940db983abdbb29c6c634ef
Kolosowski Arseny <kolosowski.a.r@gmail.com>
2022-08-18 18:02:37 +0300

    Update example

--- Example/UICenteredCollectionViewExample/View/Extensions/ViewController+SetupViews.swift
+++ Example/UICenteredCollectionViewExample/View/Extensions/ViewController+SetupViews.swift
@@ -4,6 +4,9 @@ extension ViewController {
 	
 	func setupViews() {
 		collectionView.backgroundColor = .systemPink
+		collectionView.contentInset = .init(top: 15, left: 20, bottom: 15, right: 20)
+		collectionView.layer.cornerRadius = 30
+		collectionView.layer.masksToBounds = true
 		collectionView.register(
 			CollectionViewCell.self,
 			forCellWithReuseIdentifier: String(describing: CollectionViewCell.self)
