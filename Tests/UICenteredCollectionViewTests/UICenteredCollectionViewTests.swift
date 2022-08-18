ab4d8efffd8bc7dad1e37290e3ef8764e20d45e4
Kolosowski Arseny <kolosowski.a.r@gmail.com>
2022-08-18 12:40:35 +0300

    Initial setup

---
+++ Tests/UICenteredCollectionViewTests/UICenteredCollectionViewTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import UICenteredCollectionView
+
+final class UICenteredCollectionViewTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(UICenteredCollectionView().text, "Hello, World!")
+    }
+}
