084ce17ebfc82e1fcfa50916c5b12661dbf04bc0
Kolosowski Arseny <kolosowski.a.r@gmail.com>
2022-08-18 12:41:53 +0300

    Update Package file

--- Package.swift
+++ Package.swift
@@ -4,25 +4,22 @@
 import PackageDescription
 
 let package = Package(
-    name: "UICenteredCollectionView",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "UICenteredCollectionView",
-            targets: ["UICenteredCollectionView"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "UICenteredCollectionView",
-            dependencies: []),
-        .testTarget(
-            name: "UICenteredCollectionViewTests",
-            dependencies: ["UICenteredCollectionView"]),
-    ]
+	name: "UICenteredCollectionView",
+	products: [
+		.library(
+			name: "UICenteredCollectionView",
+			targets: ["UICenteredCollectionView"]
+		),
+	],
+	dependencies: [],
+	targets: [
+		.target(
+			name: "UICenteredCollectionView",
+			dependencies: []
+		),
+		.testTarget(
+			name: "UICenteredCollectionViewTests",
+			dependencies: ["UICenteredCollectionView"]
+		),
+	]
 )
